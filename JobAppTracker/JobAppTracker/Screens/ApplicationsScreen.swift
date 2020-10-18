a3073a9438adee280d025570fbbe33436906b801
Jack Wong <jackwong.professional@gmail.com>
2020-10-18 01:35:26 -0400

    Refactored ApplicationsScreen

--- JobAppTracker/JobAppTracker/Screens/ApplicationsScreen.swift
+++ JobAppTracker/JobAppTracker/Screens/ApplicationsScreen.swift
@@ -14,39 +14,19 @@ struct ApplicationsScreen: View {
     @StateObject var jobListVM = JobListViewModel()
     
     @State private var columns = [GridItem(.adaptive(minimum: 300), spacing: 40)]
-        
+    @State private var lastSelectedJob: Job?
+    
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
             ScrollView(showsIndicators: false) {
-                LazyVGrid(columns: columns, spacing: 40) {
-                    ForEach(jobListVM.jobsList) { job in
-                        if job.id != jobListVM.selectedJob?.id {
-                            JobCard(job: job, namespace: namespace)
-                                .onTapGesture() {
-                                    jobListVM.selectedJob = job
-                                }
-                                .animation(.easeInOut) //Closing animation
-                                .zIndex(1)
-                        } else {
-                            Color.clear
-                                .frame(height: 200)
-                                .animation(.easeInOut) //Closing animation
-                        }
-                    }  /* ForEach */
-                } /* LazyVGrid */
-                .padding()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                
-                Spacer().frame(height: 70)
-            } /* ScrollView */
+                content
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                Spacer()
+                    .frame(height: 70)
+            }
             
-            VStack {
-                floatingActionButton
-                    .buttonStyle(FABStyle())
-                    .padding(.horizontal)
-        
-                Spacer().frame(height: 70)
-            } /* VStack */
+            floatingActionButton
         }  /* ZStack */
         .disabled(jobListVM.selectedJob != nil)
         .overlay(
@@ -66,16 +46,44 @@ struct ApplicationsScreen: View {
         .environmentObject(jobListVM)
     }
     
+    var content: some View {
+        LazyVGrid(columns: columns, spacing: 40) {
+            ForEach(jobListVM.jobsList) { job in
+                if job.id != jobListVM.selectedJob?.id {
+                    JobCard(job: job, namespace: namespace)
+                        .onTapGesture() {
+                            jobListVM.selectedJob = job
+                            lastSelectedJob = job
+                        }
+                        .animation(.easeInOut) //Closing animation
+                        .zIndex(job.id == lastSelectedJob?.id ? 2 : 1)
+                    /* Logic for zIndex is required to prevent card from being
+                     behind other cards when going from DetailScreen back to card */
+                } else {
+                    Color.clear
+                        .frame(height: 200)
+                        .animation(.easeInOut) //Closing animation
+                }
+            }  /* ForEach */
+        } /* LazyVGrid */
+    }
+    
     var floatingActionButton: some View {
-        Button(action: {
-            jobListVM.intent = .create
-        }, label: {
-            Image(systemName: "plus.circle.fill")
-                .resizable()
-                .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-                .frame(width: 56, height: 56)
-                .shadow(color: Color.shadow, radius: 5, x: 5, y: 5)
-        })
+        VStack {
+            Button(action: {
+                jobListVM.intent = .create
+            }, label: {
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                    .frame(width: 56, height: 56)
+                    .shadow(color: Color.shadow, radius: 5, x: 5, y: 5)
+            })
+            .buttonStyle(FABStyle())
+            .padding(.horizontal)
+            
+            Spacer().frame(height: 70)
+        }
     }
 }
 
