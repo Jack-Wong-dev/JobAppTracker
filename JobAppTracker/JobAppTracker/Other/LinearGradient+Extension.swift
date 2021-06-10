9e43a021382b5acf61cb5c0c2371cee4e90d5867
Jack <jackwong.professional@gmail.com>
2021-06-10 15:28:01 -0400

    Added LinearGradient Extension

---
+++ JobAppTracker/JobAppTracker/Other/LinearGradient+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  LinearGradient+Extension.swift
+//  JobAppTracker
+//
+//  Created by Jack Wong on 6/10/21.
+//
+
+import SwiftUI
+
+extension LinearGradient {
+    static let background = LinearGradient(gradient: Gradient(colors: [.lighting, .background]),
+                                           startPoint: .topLeading,
+                                           endPoint: .bottomTrailing)
+}
+
+
